8e530ae28ad3b04f9aba28e2e362c33106c763f2
kadochnikov.sv <kadochnikov.sv@gmail.com>
2020-08-08 17:36:41 +0300

    init repo with cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A pet project to get used to tools like make, Docker, Flask applied to Data Science tasks',
+    author='Stepan Kadochnikov',
+    license='MIT',
+)
